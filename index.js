bb942584773bd0b3fc946eee645788eaf7da9a51
vino-brutor <viktorbruno2006@gmail.com>
2024-04-28 17:31:49 -0300

    decorators

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
+// function Decorator(){
+//     return function(target, key, descriptor){ 
+//          descriptor.value = function(value: number){
+//             console.log('Calculando ', value, ' ao quadrado')
+//             return value ** 2
+//          }
+//     }
+// }
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Decorator() {
+function Log() {
     return function (target, key, descriptor) {
-        descriptor.value = function (value) {
-            console.log('Calculando ', value, ' ao quadrado');
-            return Math.pow(value, 2);
+        const originalMethod = descriptor.value; //o key guarda o nome do método/função original
+        descriptor.value = function (...args) {
+            console.log('.....................................');
+            console.log(`Chamando o método ${key} com os parâmetros ${JSON.stringify(args)}`);
+            const result = originalMethod.apply(this, args);
+            console.log(`O método ${key} retornou o valor: ${JSON.stringify(result)}`);
+            console.log('.....................................');
+            return result;
         };
     };
 }
@@ -20,10 +33,17 @@ class Planet {
         console.log('Calculando ', value, ' vezes 2');
         return value * 2;
     }
+    InvertName() {
+        return this.name.split('').reverse().join('');
+    }
 }
 __decorate([
-    Decorator()
+    Log()
 ], Planet.prototype, "calculate", null);
+__decorate([
+    Log()
+], Planet.prototype, "InvertName", null);
 const planet = new Planet('terra');
 const result = planet.calculate(4);
 console.log('resultado igual a ', result);
+planet.InvertName();
--- index.ts
+++ index.ts
@@ -1,12 +1,30 @@
-function Decorator(){
+// function Decorator(){
+//     return function(target, key, descriptor){ 
+//          descriptor.value = function(value: number){
+//             console.log('Calculando ', value, ' ao quadrado')
+//             return value ** 2
+//          }
+//     }
+// }
+
+
+function Log(){
     return function(target, key, descriptor){ //descriptor equivale a propria função q foi decorada, no caso a calculate
-         descriptor.value = function(value: number){
-            console.log('Calculando ', value, ' ao quadrado')
-            return value ** 2
-         }
+        const originalMethod = descriptor.value //o key guarda o nome do método/função original
+
+        descriptor.value = function(...args: any[]){
+            console.log('.....................................')
+            console.log(`Chamando o método ${key} com os parâmetros ${JSON.stringify(args)}`)
+
+            const result = originalMethod.apply(this, args)
+
+            console.log(`O método ${key} retornou o valor: ${JSON.stringify(result)}`)
+            console.log('.....................................')
+
+            return result
+        }
     }
 }
-
 class Planet{
     name:string
 
@@ -14,10 +32,15 @@ class Planet{
         this.name = name
     }
 
-    @Decorator()
+    @Log()
     calculate(value:number){
         console.log('Calculando ', value, ' vezes 2')
         return value * 2
+    }
+
+    @Log()
+    InvertName(){
+        return this.name.split('').reverse().join('')
     }
 }
 
@@ -25,4 +48,7 @@ const planet = new Planet('terra')
 
 const result = planet.calculate(4)
 
+
 console.log('resultado igual a ', result)
+
+planet.InvertName()
